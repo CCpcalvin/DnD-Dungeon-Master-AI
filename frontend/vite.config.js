5abdec9bbea3d7d9d0aad99958c6bc10f5be55dc
CCpcalvin <chunpangchan2001@gmail.com>
2025-07-06 13:42:20 +0800

    update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-const basename = import.meta.env.VITE_BASE_URL || "/";
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  // Now you can access the environment variables via env.VARIABLE_NAME
+  const basename = env.VITE_BASE_URL || "/";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    watch: {
-      usePolling: true, // Use polling for file changes
+  return {
+    plugins: [react()],
+    server: {
+      host: "0.0.0.0",
+      watch: {
+        usePolling: true, // Use polling for file changes
+      },
     },
-  },
-  base: basename,
+    base: basename,
+  };
 });
